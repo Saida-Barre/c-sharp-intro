8ce632a1ec6d9b6c7548e55971f0bc185086890e
unknown <sbarre33@gmail.com>
2020-06-25 09:36:49 -0700

    Add datatype examples; added mathematical operators.

--- Program.cs
+++ Program.cs
@@ -5,8 +5,35 @@ namespace c_sharp_intro
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { // "=" is the assignment operator!
+         // String datatype is used for text.
+          string myString = "This is a string!";
+          // Integer datatype is used for whole numbers (no decimals.)
+          int myInt = 42;
+          // Double datatype is used for decimal point numbers.
+          double myDouble = 33.666; 
+          // Boolean datatype is used for TRUE/FALSE values - think binary!
+          bool myBool = true;
+
+          // Subtraction operator is "-" (minus.)
+          Console.WriteLine( myInt - 10) ; //32
+
+          //Multiplication operator is "*" (multiply.)
+          Console.WriteLine( myInt * 2 ); // 84
+
+          // Division operator is "/" (division.)
+          Console.WriteLine( myInt / 2); // 21
+
+          // Modulus operator is "%" (percent sign.)
+          Console.WriteLine(myInt % 13 ); //
+
+          // Addition operator is "+" (plus.)
+          //***WAIT! This is ALSO the concatenation operator.
+          Console.WriteLine( myInt + 8); //50
+
+          Console.WriteLine("3" + "3"); //33
+          
+
         }
     }
 }
